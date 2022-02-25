67e6d336bd11ebdb4d4c61659c2fb395356b8178
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-02-25 15:55:52 +0300

    style: add javascript interaction with the experience tab links

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let crescent360Link = document.querySelector(".crescent");
+let iCreationsLink = document.querySelector(".create");
+let iAmBrandLink = document.querySelector(".brand");
+let crescentJd = document.querySelector(".Crescent360");
+let icreationsJd = document.querySelector(".Icreations");
+let iAmbrandJd = document.querySelector(".IamBrand");
+
+crescent360Link.addEventListener("click", () => {
+    crescent360Link.classList.add("active-link");
+    iCreationsLink.classList.remove("active-link");
+    iAmBrandLink.classList.remove("active-link");
+
+    crescentJd.classList.add("active-description");
+    icreationsJd.classList.remove("active-description");
+    iAmbrandJd.classList.remove("active-description");
+});
+
+iCreationsLink.addEventListener("click", () => {
+    crescent360Link.classList.remove("active-link");
+    iCreationsLink.classList.add("active-link");
+    iAmBrandLink.classList.remove("active-link");
+
+    crescentJd.classList.remove("active-description");
+    icreationsJd.classList.add("active-description");
+    iAmbrandJd.classList.remove("active-description");
+});
+
+iAmBrandLink.addEventListener("click", () => {
+    crescent360Link.classList.remove("active-link");
+    iCreationsLink.classList.remove("active-link");
+    iAmBrandLink.classList.add("active-link");
+
+    crescentJd.classList.remove("active-description");
+    icreationsJd.classList.remove("active-description");
+    iAmbrandJd.classList.add("active-description");
+});
